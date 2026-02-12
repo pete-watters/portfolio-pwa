0e66fd55eefc07e875599ce142aa9de0b9792f47
Peter Watters <2938440+pete-watters@users.noreply.github.com>
2026-02-12 05:45:42 +0000

    feat: migrate from CRA + Firebase to Astro + Cloudflare Pages
    Replace the broken React 16 / CRA site with Astro 5 static output.
    Adds markdown blog via content collections, PWA support via
    @vite-pwa/astro, Vitest + Playwright testing, and GitHub Actions
    CI/CD for lint/typecheck/test/deploy to Cloudflare Pages.

--- src/components/Page.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Page from './Page';
-
-describe('Page', () => {
-  it('should render the component correctly', () => {
-    expect(shallow(<Page />)).toMatchSnapshot();
-  });
-});
--- src/components/Section.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Section from './Section';
-
-describe('Section', () => {
-  it('should render the component correctly', () => {
-    expect(shallow(<Section />)).toMatchSnapshot();
-  });
-});
